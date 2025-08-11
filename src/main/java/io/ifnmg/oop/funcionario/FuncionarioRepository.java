928d8c38761de64ae974bf83b34933c95a6dfc0c
Pedro Bazzarella <pedrobazzarella@gmail.com>
2025-08-11 10:13:19 -0300

    Feature: Funcionario repository

---
+++ src/main/java/io/ifnmg/oop/funcionario/FuncionarioRepository.java
@@ -0,0 +1,11 @@
+package io.ifnmg.oop.funcionario;
+
+import io.ifnmg.oop.repository.Repository;
+
+public class FuncionarioRepository
+        extends Repository<Funcionario> {
+
+    public FuncionarioRepository(){
+        super();
+    }
+}
