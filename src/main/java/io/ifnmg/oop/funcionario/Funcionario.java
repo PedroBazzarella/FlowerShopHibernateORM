3cd2a36c93d64db75b3336bd01872cadfd309b9f
matheusAntunesF <matheusantunesfreire.123@gmail.com>
2025-08-10 21:06:34 -0300

    Funcionario class created

---
+++ src/main/java/io/ifnmg/oop/funcionario/Funcionario.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.ifnmg.oop.funcionario;
+
+import io.ifnmg.oop.repository.ProjectEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author Matheus Antunes <maf6@aluno.ifnmg.edu.br>
+ */
+@Entity
+public class Funcionario 
+        extends ProjectEntity
+        implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Funcionario)) {
+            return false;
+        }
+        Funcionario other = (Funcionario) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "io.ifnmg.oop.funcionario.Funcionario[ id=" + id + " ]";
+    }
+    
+}
